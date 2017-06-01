10d12acf078cdf79dfb4a57e3bb56534c47382cc
Mallerie Shirley <mallerie@gmail.com>
2017-06-01 10:19:35 -0400

    Added App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Link from './Link.js';
+
 
 class App extends Component {
   render() {
@@ -13,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Link>Test Link To Nowhere</Link>
       </div>
     );
   }
